b688e445b71f29fb28cb191b554fd21de47c749e
Onkar Shedge <oshedge@outlook.com>
2020-11-25 12:48:19 +0530

    Add histogram metric for http requests

--- src/main/java/com/example/hellometric/Greetings.java
+++ src/main/java/com/example/hellometric/Greetings.java
@@ -4,12 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/greeting")
 public class Greetings {
+    Random random;
+    double mean = 3;
+    double standardDeviation = 2;
 
-    @GetMapping
-    public String hello() {
-        return "hola world !!";
+    public Greetings() {
+        random = new Random();
+    }
+
+//    @GetMapping
+//    public double hello() throws InterruptedException {
+//        double randomValue = mean + random.nextGaussian() * standardDeviation;
+//        if(randomValue > 0) {
+//            Thread.sleep(Math.round(randomValue) * 1000);
+//        }
+//        return randomValue;
+//    }
+
+    @GetMapping()
+    public double helloRandom() throws InterruptedException {
+        int randomValue = random.nextInt((5000 - 50) + 50);
+        if (randomValue > 0) {
+            Thread.sleep(Math.round(randomValue));
+        }
+        return randomValue;
     }
 }
